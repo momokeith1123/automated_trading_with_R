a96d3c260bff164ac53f35de7ed254a6a0b3c92d
Mamadou Oumar Keïta <m.keita@afdb.org>
2021-03-24 18:32:21 +0000

    initial commit

---
+++ eikon.R
@@ -0,0 +1,27 @@
+eikon_get <-
+function(sym, sdate = "2000-01-01T16:45:00", edate = current ) {
+    require(tidyverse)
+    require(devtools)
+    require(eikonapir)
+    require(lubridate)
+    require (xts)
+    # create a vector with all linesye
+   dt <- tryCatch(get_timeseries(sym,fields = "*",
+                            start_date = sdate ,
+                            end_date =  edate,
+                            debug = FALSE)) 
+  
+    df <- tibble (  'Dates'= ymd(substr(as.character(dt$TIMESTAMP), start = 1, stop = 10)),
+                    'OPEN' = as.double(dt$OPEN),
+                    'HIGH' = as.double(dt$HIGH),
+                    'LOW'  = as.double(dt$LOW),
+                    'CLOSE' = as.double(dt$CLOSE),
+                    'COUNT' = as.double(dt$COUNT),
+                    'VOLUME' = as.double(dt$VOLUME),
+                    'RIC'  = sym
+                   )
+    # names(df )[2:6] <- paste0( paste0(names(df )[2:6], '.', sym))
+    
+    return(df)
+
+}
