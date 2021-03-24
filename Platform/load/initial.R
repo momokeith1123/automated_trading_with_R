d2a641d3848b3ca1f709455d1532a7e788be4485
Mamadou Oumar Keïta <m.keita@afdb.org>
2021-03-24 18:34:51 +0000

    stock

--- Platform/load/initial.R
+++ Platform/load/initial.R
@@ -1,13 +1,13 @@
 # Listing 2.4
 setwd(DIR[["function"]])
-source("yahoo.R")
+source("eikon.R")
 
 setwd(DIR[["root"]])
 if("S.R" %in% list.files()) {
   source("S.R")
 } else {
-  url <- "http://trading.chrisconlan.com/SPstocks.csv"
-  S <- as.character(read.csv(url, header = FALSE)[,1])
+  stocksFile <- paste0(DIR[["data"]], "/", "SPric.csv")
+  S <- as.character(read.csv( stocksFile, header = FALSE)[,1])
   dump(list = "S", "S.R")
 }
 
@@ -18,22 +18,18 @@ setwd(DIR[["data"]])
 toload <- setdiff(S[!paste0(S, ".csv") %in% list.files()], invalid)
 
 if(length(toload) != 0){
-  for(i in 1:length(toload)){
-    
-  df <- yahoo(toload[i])
-  
-  if(!is.null(df)) {
-    write.csv(df[nrow(df):1], file = paste0(toload[i], ".csv"),
-              row.names = FALSE) 
-  } else {
-    invalid <- c(invalid, toload[i])
+  for (i in seq_along(toload)) {
+    df <- eikon_get(toload[i])
+    if (!is.null(df)) {
+      write_csv(df,  paste0(toload[i], ".csv"))
+    } else {
+      invalid <- c(invalid, toload[i])
+    }
   }
-    
 }
-}
-
 setwd(DIR[["root"]])
 dump(list = c("invalid"), "invalid.R")
 
 rm(list = setdiff(ls(), c("CONFIG", "DIR", "yahoo")))
 gc()
+
