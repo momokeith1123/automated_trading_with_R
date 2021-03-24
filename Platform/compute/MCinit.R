86be382e52c3e8d124156036198d5c6bdfc6ddb6
Mamadou Oumar Keïta <m.keita@afdb.org>
2021-03-24 18:33:07 +0000

    rewrite date

--- Platform/compute/MCinit.R
+++
@@ -1,9 +0,0 @@
-if( CONFIG[["isUNIX"]] ){
-  library(doMC)
-  workers <- CONFIG[["workers"]]
-  registerDoMC( cores = workers )
-} else {
-  library(doParallel)
-  workers <- CONFIG[["workers"]]
-  registerDoParallel( cores = workers )
-}
--- Platform/functions/eikon.R
+++ Platform/functions/eikon.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(devtools)
 library(eikonapir)
 
@@ -10,14 +11,15 @@ eikon_api = "9a548e9c62cb499b8ebd0d9a777b4d857ef449a0"
 #add my key
 eikonapir::set_app_id(eikon_api)
 result <- get_symbology(list("MSFT.O", "GOOG.O", "IBM.N"),"RIC",list("ISIN"),raw_ouput = FALSE,debug=FALSE)
-df = get_timeseries(list("MSFT.O","VOD.L","IBM.N"),list("*"),"2016-01-01T15:04:05","2016-01-10T15:04:05","daily")
+# df = get_timeseries("MSFT.O",list("*"),start_date = "2016-01-01T15:04:05",end_date = "2021-03-22T15:04:05",interval = "daily", raw_output = TRUE)
+dat = get_timeseries("MSFT.O",list("*"),start_date = "2016-01-04T00:00:00Z",end_date ="2021-03-22T00:00:00Z","daily")
 
 
-y <- as.numeric(substr(as.character(Sys.time()), start = 1, stop = 4))
-m <- as.numeric(substr(as.character(Sys.time()), start = 6, stop = 7)) 
-d <- as.numeric(substr(as.character(Sys.time()), start = 9, stop = 10))
+y <- substr(as.character(Sys.time()), start = 1, stop = 4)
+m <- substr(as.character(Sys.time()), start = 6, stop = 7) 
+d <- substr(as.character(Sys.time()), start = 9, stop = 10)
 
-current <- paste0(y, "-", m, "-", d, "T16:45:00")
+current <- paste0(y, "-", m, "-", d, "T00:00:00Z")
 
 eikon_get <-
 function(sym, sdate = "2000-01-01T16:45:00", edate = current ) {
@@ -32,7 +34,7 @@ function(sym, sdate = "2000-01-01T16:45:00", edate = current ) {
                             end_date =  edate,
                             debug = FALSE)) 
   
-    df <- tibble (  'Dates'= ymd(substr(as.character(dt$TIMESTAMP), start = 1, stop = 10)),
+    df <- tibble (  'DATE'= ymd(substr(as.character(dt$TIMESTAMP), start = 1, stop = 10)),
                     'OPEN' = as.double(dt$OPEN),
                     'HIGH' = as.double(dt$HIGH),
                     'LOW'  = as.double(dt$LOW),
@@ -47,3 +49,9 @@ function(sym, sdate = "2000-01-01T16:45:00", edate = current ) {
 
 }
 
+
+
+
+# S <- list("MSFT.O", "GOOG.O","IBM.N") %>% set_names(c("MSFT.O", "GOOG.O","IBM.N"))
+# 
+# dat <- map(S,~ eikon_get(.x))
